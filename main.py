f0e6048039119ae09139662bef41ea2bd4f72d66
RamalaAshok <64201236+RamalaAshok@users.noreply.github.com>
2020-04-23 17:02:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Program for addition ,subtraction and Multiplication
+a =int(input("Enter the value :"))
+b =int(input("Enter the value :"))
+#Addition of  two numbers
+c = a + b
+print (c)
+#subtracting two  numbers
+d= a-b
+print(d)
+#Multiply two numbers
+E = a*b
+print(E)
